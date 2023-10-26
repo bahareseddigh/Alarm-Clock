f3d2a8816f0b8ec212be9d7beb93478bd0b956c3
bahare <pondblue07@gmail.com>
2023-10-26 16:19:37 +0330

    used setInterval method for changing time

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const selectMenu = document.querySelectorAll('select')
+const timeBox = document.querySelector('.time')
+
 for(let i = 23 ; i >= 0 ; i--){
     i = i < 10 ? '0' + i : i
     let option = `<option value="${i}">${i}</option>`
@@ -9,3 +11,17 @@ for(let i = 59 ; i >= 0 ; i--){
     let option = `<option value="${i}">${i}</option>`
     selectMenu[1].firstElementChild.insertAdjacentHTML('afterend' , option)
 }
+
+setInterval( () => {
+    let date = new Date()
+    let hour = date.getHours()
+    let minute = date.getMinutes()
+    let seconds = date.getSeconds()
+
+    hour = hour < 10 ? '0' + hour : hour
+    minute = minute < 10 ? '0' + minute : minute
+    seconds = seconds < 10 ? '0' + seconds : seconds
+    timeBox.innerHTML = `${hour}:${minute}:${seconds}`
+    ;
+
+}, 1000)
