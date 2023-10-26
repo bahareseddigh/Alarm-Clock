3e9269b86222669741f43c45586e0c2058921bff
bahare <pondblue07@gmail.com>
2023-10-26 16:02:32 +0330

    minute's select menu implemented in script.js

--- script.js
+++ script.js
@@ -4,3 +4,8 @@ for(let i = 23 ; i >= 0 ; i--){
     let option = `<option value="${i}">${i}</option>`
     selectMenu[0].firstElementChild.insertAdjacentHTML('afterend' , option)
 }
+for(let i = 59 ; i >= 0 ; i--){
+    i = i < 10 ? '0' + i : i
+    let option = `<option value="${i}">${i}</option>`
+    selectMenu[1].firstElementChild.insertAdjacentHTML('afterend' , option)
+}
